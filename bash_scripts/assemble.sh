be6f6081780d1cd89c0297d3e3a1ab34032e763c
Lucy <94082171+Lucy-Quirk@users.noreply.github.com>
2024-05-01 14:54:30 -0400

    Update assemble.sh

--- bash_scripts/assemble.sh
+++ bash_scripts/assemble.sh
@@ -8,13 +8,15 @@
 #SBATCH --output=%x.out
 #SBATCH --error=%x.err
 
-#using conda environment spades 
+# activate conda environment spades 
+# this script loops through each organism number, and uses the respective .yml file
+# containing sequence file names, locations, and sequence types (Illumina vs PacBio or forward vs reverse read)
 
 samples="04 06 08 13"
 
 for s in `echo $samples`; do
 	echo ${s}
  
-	rnaspades.py --dataset input${r}${s}.yaml -o /path/to/Assembly/${s}/rnaSpades/
+	rnaspades.py --dataset input${s}.yaml -o /path/to/Assembly/${s}/
 done;
 
